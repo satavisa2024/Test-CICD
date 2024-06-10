cc7d28f8a9106d36081219773dce4b0c3c621462
Shamik Bhattacharya <shamikbhattacharya@navneettoptech.com>
2024-06-10 16:16:31 +0530

    first commit

---
+++ src/main/java/com/example/Test_CICD/TestCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.Test_CICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Test_CICD/TestCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Test_CICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
